9d95998b3cb8f687adbfd491712b9a2d86d67bec
Allan Degnan <allan@secureweb.ltd>
2020-10-05 10:59:02 +0100

    added bunch of extra charts

---
+++ modules/grafana/main.tf
@@ -0,0 +1,12 @@
+resource "kubernetes_namespace" "grafana" {
+  metadata {
+    name = "grafana"
+  }
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = kubernetes_namespace.grafana.metadata.0.name
+}
