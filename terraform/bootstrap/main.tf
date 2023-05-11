1959ebed11013787472eb6fa5d62d31930ce2d6f
Allan Degnan <allan@secureweb.ltd>
2023-05-11 07:35:37 +0100

    test

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -3,3 +3,20 @@ module "atlantis" {
   
   yaml = var.github_yaml
 }
+
+
+resource "kubernetes_cluster_role_binding" "cluster_role_binding" {
+  metadata {
+    name = "atlantis-superpowers"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "atlantis"
+    namespace = "atlantis"
+  }
+}
