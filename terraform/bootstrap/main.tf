4bed001899891280210b7379d8d169f09120bc97
Allan Degnan <allan@secureweb.ltd>
2023-06-27 14:40:01 +0100

    test

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -3,8 +3,7 @@ resource "kubernetes_secret_v1" "input_vars" {
     name = "input-vars"
   }
   data = {
-    github-app            = var.github-app
-    atlantis              = var.atlantis
+    atlantis-github-app   = var.atlantis-github-app
     github-webhook-secret = var.github-webhook-secret
     arc-github-app        = var.arc-github-app
   }
