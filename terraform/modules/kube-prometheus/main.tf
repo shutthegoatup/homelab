29e251b56e39da23cfeb34607a3d6b0c8824d952
Allan Degnan <allan@secureweb.ltd>
2023-07-05 08:58:15 +0100

    added sso for grafana

--- terraform/modules/kube-prometheus/main.tf
+++ terraform/modules/kube-prometheus/main.tf
@@ -5,6 +5,7 @@ resource "kubernetes_namespace" "ns" {
 }
 
 resource "helm_release" "helm" {
+  depends_on    = [helm_release.secrets]
   wait          = true
   wait_for_jobs = true
   name          = "kube-prometheus-stack"
@@ -16,4 +17,33 @@ resource "helm_release" "helm" {
     grafana-service-name = var.grafana_service_name,
     fqdn                 = var.fqdn
   })]
+}
+
+resource "helm_release" "secrets" {
+  for_each      = toset(var.secrets)
+  wait          = true
+  wait_for_jobs = true
+  name          = each.key
+  repository    = "https://dysnix.github.io/charts"
+  chart         = "raw"
+  version       = "0.3.1"
+  namespace     = kubernetes_namespace.ns.metadata.0.name
+  values = [
+    <<-EOF
+    resources:
+    - apiVersion: secrets.hashicorp.com/v1beta1
+      kind: VaultStaticSecret
+      metadata:
+        namespace: ${var.namespace}
+        name: ${each.key}
+      spec:
+        mount: "kvv2"
+        type: kv-v2
+        path: ${each.key}
+        refreshAfter: 60s
+        destination:
+          create: true
+          name: ${each.key}
+          EOF
+  ]
 }
--- terraform/modules/kube-prometheus/values.yaml.tpl
+++ terraform/modules/kube-prometheus/values.yaml.tpl
@@ -7,3 +7,29 @@ grafana:
       - ${grafana-service-name}.${fqdn}
     path: /
     pathType: Prefix
+  grafana.ini:
+    server:
+      root_url: https://${grafana-service-name}.${fqdn}
+    auth:
+      disable_login_form: true
+    auth.google:
+      enabled: true
+      allow_sign_up: true
+      auto_login: true
+      client_id: notsecret
+      client_secret: notsecret
+      scopes: https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email
+      auth_url: https://accounts.google.com/o/oauth2/auth
+      token_url: https://accounts.google.com/o/oauth2/token
+      allowed_domains: ${fqdn}
+      hosted_domain: ${fqdn}
+      use_pkce: true
+  envValueFrom:
+    GF_AUTH_GOOGLE_CLIENT_ID:
+      secretKeyRef:
+        name: gsuite
+        key: client-id
+    GF_AUTH_GOOGLE_CLIENT_SECRET:
+      secretKeyRef:
+        name: gsuite
+        key: client-secret
